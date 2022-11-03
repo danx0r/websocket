e97c40d137685a42cbccba0954c996e03bb596ba
danx0r <dan.miller@eye0.com>
2022-11-03 14:46:36 -0700

    Good Ol' Fashion Javascript

---
+++ ws_client.js
@@ -0,0 +1,25 @@
+let socket = new WebSocket("ws://localhost:1234");
+  socket.onopen = function(e) {
+  console.log("[open] Connection established");
+  console.log("Sending to server");
+  socket.send("My name is danx0r");
+};
+
+socket.onmessage = function(event) {
+  console.log(`[message] Data received from server: ${event.data}`);
+  document.getElementById("message").innerHTML = event.data;
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+  } else {
+    // e.g. server process killed or network down
+    // event.code is usually 1006 in this case
+    console.log('[close] Connection died');
+  }
+};
+
+socket.onerror = function(error) {
+  console.log(`[error] ${error.message}`);
+};
