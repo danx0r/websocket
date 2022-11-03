ace75f5c058ab0c34240e2cb60c4231c58d91690
danx0r <dan.miller@eye0.com>
2022-11-03 15:29:11 -0700

    Formattiin

--- ws_client_ts.ts
+++ ws_client_ts.ts
@@ -1,25 +1,23 @@
-let socket = new WebSocket("ws://localhost:1234");
-  socket.onopen = function(e) {
-  console.log("[open] Connection established");
-  console.log("Sending to server");
-  socket.send("My name is danx0r");
+var socket = new WebSocket("ws://localhost:1234");
+socket.onopen = function (e) {
+    console.log("[open] Connection established");
+    console.log("Sending to server");
+    socket.send("My name is danx0r");
 };
-
-socket.onmessage = function(event) {
-  console.log(`[message] Data received from server: ${event.data}`);
-  document.getElementById("message").innerHTML = event.data;
+socket.onmessage = function (event) {
+    console.log("[message] Data received from server: ".concat(event.data));
+    document.getElementById("message").innerHTML = event.data;
 };
-
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    console.log('[close] Connection died');
-  }
+socket.onclose = function (event) {
+    if (event.wasClean) {
+        console.log("[close] Connection closed cleanly, code=".concat(event.code, " reason=").concat(event.reason));
+    }
+    else {
+        // e.g. server process killed or network down
+        // event.code is usually 1006 in this case
+        console.log('[close] Connection died');
+    }
 };
-
-socket.onerror = function(error) {
-  console.log(`[error] ${error.message}`);
+socket.onerror = function (error) {
+    console.log("[error] ".concat(error.message));
 };
